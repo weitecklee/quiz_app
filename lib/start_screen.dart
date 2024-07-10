0ca7ab981c1c28a4989035bbb4fd47fa4743e64e
arkteck <arkteck@gmail.com>
2024-07-10 15:49:23 -0400

    some styling

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -11,25 +11,28 @@ class StartScreen extends StatelessWidget {
         children: [
           Image.asset(
             'assets/images/quiz-logo.png',
-            width: 250.0,
+            width: 300.0,
+            color: const Color.fromARGB(180, 255, 255, 255),
           ),
-          const SizedBox(height: 50),
+          const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
             style: TextStyle(
-              fontSize: 20,
+              fontSize: 24,
               color: Colors.white,
             ),
           ),
-          const SizedBox(height: 20),
-          OutlinedButton(
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
             onPressed: () {},
-            style: const ButtonStyle(),
-            child: const Text(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
               'Start Quiz',
               style: TextStyle(
                 fontSize: 18,
-                color: Colors.white,
               ),
             ),
           )
